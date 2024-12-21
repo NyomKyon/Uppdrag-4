0f10e753313c64d6a55ce2448b05488d75329eaa
Lekzy <nyomkyon@gmail.com>
2024-12-21 22:41:24 +0100

    footer margins mm

--- menu.js
+++ menu.js
@@ -1,10 +1,12 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menu").style.display = "none";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu").style.display = "block"
   }
 
